97bb4c1073c98c6bdeac469d58f67c8d09d741d3
RichardJab <ricardojlm1215@gmail.com>
2015-05-26 13:16:02 -0500

    Suma de a y b

--- Hola GIT/Program.cs
+++ Hola GIT/Program.cs
@@ -9,6 +9,10 @@ namespace HelloWorld
         static void Main() 
         {
             Console.WriteLine("Hola GIT!");
+            int a = 1;
+            int b = 2;
+
+            Console.WriteLine("La suma es {0}", a + b);
 
             // Mantiene la consola abierta
             Console.WriteLine("Pulsar cualquier tecla para salir.");
